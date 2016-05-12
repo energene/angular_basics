b083907e3abec44f414e17ed03d19b64136392a5
Gene Troy <genetroy@hotmail.com>
2016-05-12 13:11:30 -0700

    add sourcemap, refactor a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,40 @@
 const gulp = require('gulp');
-// const exec = require('child_process').exec;
 const webpack = require('webpack-stream');
-// const nodemon = require('gulp-nodemon');
 const eslint = require('gulp-eslint');
-// const protractor = require('gulp-protractor').protractor;
 
-gulp.task('backend:lint', () => {
+gulp.task('lint:backend', () => {
   gulp.src('./**/*.js')
-    .pipe(eslint());
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
-gulp.task('client:lint', () => {
-  gulp.src('./**/*.html')
-    .pipe(eslint());
+gulp.task('lint:client', () => {
+  gulp.src('./app/index.html')
+  .pipe(eslint())
+  .pipe(eslint.format());
 });
 
 gulp.task('webpack:dev', () => {
-  gulp.src('app/js/entry.js')
-    .pipe(webpack({
-      output: {
-        filename: 'bundle.js'
-      }
-    }))
-    .pipe(gulp.dest('./build'));
+  gulp.src('./app/js/entry.js')
+  .pipe(webpack({
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        { test: /\.css$/, loader: 'style!css' }
+      ]
+    },
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('./build'));
 });
 
 gulp.task('static:dev', () => {
-  gulp.src('app/**/*.html')
-    .pipe(gulp.dest('./build'));
+  gulp.src('./app/**/*.html')
+  .pipe(gulp.dest('./build'));
 });
 
-// gulp.task('protractor', () => {
-//   gulp.src(["./src/tests/*.js"])
-//     .pipe(protractor({
-//         configFile: "test/protractor.config.js",
-//         args: ['--baseUrl', 'http://127.0.0.1:8000']
-//     }))
-//     .on('error', function(e) { throw e })
-// });
-
+gulp.task('lint', ['lint:backend', 'lint:client']);
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);
-gulp.task('default', ['backend:lint', 'client:lint', 'build:dev']);
-// gulp.task('default', ['backend:lint', 'client:lint', 'build:dev', 'start', 'protractor']);
+gulp.task('default', ['lint', 'build:dev']);
